aeee3eee084d3f8e91edb0a06f232354071c8266
FullStackDeveloperShubham <shubhamfullstackdeveloper@gmail.com>
2025-08-24 13:04:38 +0530

    code optimise for api response in utils

--- src/controllers/healthCheck.controllers.js
+++ src/controllers/healthCheck.controllers.js
@@ -1,10 +1,21 @@
 import { ApiResponse } from "../utils/api-response.js";
+import { asynHandler } from "../utils/asyn-handler.js";
 
-const healthCheck = (req, res) => {
+/** 
+const healthCheck = (req, res, next) => {
   try {
     res
       .status(200)
       .json(new ApiResponse(200, { message: "Server is running" }));
   } catch (error) {}
 };
+*/
+
+const healthCheck = asynHandler(async (req, res) => {
+  res.status(200).json(
+    new ApiResponse(200, {
+      message: "Server is running",
+    }),
+  );
+});
 export { healthCheck };
